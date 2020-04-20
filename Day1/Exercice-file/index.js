e043e80bd43f0ee72cdc0d2733ad1aaeb0cca0da
lasridouae <lasridouae1@gmail.com>
2020-04-20 15:53:41 +0000

    workshop number1 completed

---
+++ Day1/Exercice-file/index.js
@@ -0,0 +1,54 @@
+//1. Evaluation
+
+console.log(eval(5 + "34"));
+
+console.log(eval(5 - "4"));
+
+console.log(eval(10 % 5));
+
+console.log(eval(3 - 4));
+
+console.log(eval(" " + 0));
+
+console.log(eval("Bob" - "bill"));
+
+console.log(eval(" " + " "));
+
+console.log(eval(true + true));
+
+console.log(eval(true + false));
+
+console.log(eval(false + true));
+
+console.log(eval(false - true));
+
+let x ="java";
+let y = "script"
+console.log(x+y);
+
+//2. Evaluate the comparisons
+
+console.log(eval(5 >= 1));
+
+console.log(eval(0 === 1));
+
+console.log(eval(4 <= 1));
+
+console.log(eval(1 != 1));
+
+console.log(eval("A" > "B"));
+
+console.log(eval("B" < "C"));
+
+console.log(eval("a" > "A"));
+
+console.log(eval("b" < "A"));
+
+console.log(eval(true === false));
+
+console.log(eval(true != true));
+
+//3. making the string into a sentence
+
+
+console.log("hello!"+"its"+"rainy"+"out");
